52b28c6a717a39a7e7ad0e97eaf09df99a4d3455
MahinurHabib02 <mahinurhabib@gmail.com>
2026-01-09 04:44:55 +0600

    db constraction setup

--- Project personal/MVC/Model/AdminDashboardModel.php
+++ Project personal/MVC/Model/AdminDashboardModel.php
@@ -1,2 +1,12 @@
 <?php
 require_once __DIR__ . "/DB.php";
+
+class AdminDashboardModel {
+    private $conn;
+
+    public function __construct() {
+        $db = new DataBase();
+        $this->conn = $db->connect();
+    }
+
+}
