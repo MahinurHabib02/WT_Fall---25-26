cd8e16886ea7408057e9bc11e0673ea0f6f5e629
MahinurHabib02 <mahinurhabib@gmail.com>
2026-01-09 04:48:33 +0600

    creat function  take data from data bas

--- Project personal/MVC/Model/AdminDashboardModel.php
+++ Project personal/MVC/Model/AdminDashboardModel.php
@@ -9,4 +9,24 @@ class AdminDashboardModel {
         $this->conn = $db->connect();
     }
 
+    public function totalUsers() {
+        $res = $this->conn->query("SELECT COUNT(*) as total FROM users");
+        return $res->fetch_assoc()['total'];
+    }
+
+    public function totalVolunteers() {
+        $res = $this->conn->query("SELECT COUNT(*) as total FROM users WHERE role='volunteer'");
+        return $res->fetch_assoc()['total'];
+    }
+
+    public function activeEvents() {
+        $res = $this->conn->query("SELECT COUNT(*) as total FROM events WHERE status='Active'");
+        return $res->fetch_assoc()['total'];
+    }
+
+    public function upcomingEvents() {
+        return $this->conn->query(
+            "SELECT * FROM events WHERE date >= CURDATE() ORDER BY date ASC LIMIT 5"
+        );
+    }
 }
