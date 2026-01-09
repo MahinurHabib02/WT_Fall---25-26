629a1e95733e232b96882d9efd414f13bb3d8a66
MahinurHabib02 <mahinurhabib@gmail.com>
2026-01-10 05:10:39 +0600

    class creat db connection

--- Project personal/MVC/Model/AdminUsersModel.php
+++ Project personal/MVC/Model/AdminUsersModel.php
@@ -1,2 +1,12 @@
 <?php
 require_once __DIR__ . "/DB.php";
+
+class AdminUsersModel {
+    private $conn;
+
+    public function __construct() {
+        $db = new Database();
+        $this->conn = $db->connect();
+    }
+
+}
