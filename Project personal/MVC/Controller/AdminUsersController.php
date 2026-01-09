8646ce5e39edbea0cd41471c601774cc3af36376
MahinurHabib02 <mahinurhabib@gmail.com>
2026-01-10 05:27:40 +0600

    object creat and delete logic creat

--- Project personal/MVC/Controller/AdminUsersController.php
+++ Project personal/MVC/Controller/AdminUsersController.php
@@ -5,3 +5,14 @@ require_once __DIR__ . "/../Model/AdminUsersModel.php";
 if(!isset($_SESSION['username'])){
     $_SESSION['username'] = "Admin";
 }
+
+$model = new AdminUsersModel();
+
+
+if(isset($_GET['delete_id'])){
+    $model->deleteUser($_GET['delete_id']);
+    header("Location: /Project personal/MVC/Controller/AdminUsersController.php");
+    exit;
+}
+
+
