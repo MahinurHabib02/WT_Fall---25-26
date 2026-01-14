5b07ee376a733a6dbaa03ea7840fd440ee8423ca
MahinurHabib02 <mahinurhabib@gmail.com>
2026-01-14 06:07:50 +0600

    ad user  controller fix

--- Project personal/MVC/Controller/AdminUsersController.php
+++ Project personal/MVC/Controller/AdminUsersController.php
@@ -1,29 +1,34 @@
 <?php
 session_start();
-require_once __DIR__ . "/../Model/AdminUsersModel.php";
+require_once __DIR__ . "../../Model/AdminUsersModel.php";
 
-if(!isset($_SESSION['username'])){
-    $_SESSION['username'] = "Admin";
+if(!isset($_SESSION['username']))
+    {
+    $_SESSION['username'] = $user['username'];
 }
 
 $model = new AdminUsersModel();
 
 
-if(isset($_GET['delete_id'])){
+if(isset($_GET['delete_id']))
+    {
     $model->deleteUser($_GET['delete_id']);
-    header("Location: /Project personal/MVC/Controller/AdminUsersController.php");
+    header("Location: ../Controller/AdminUsersController.php");
     exit;
 }
 
-if(isset($_POST['update_user'])){
+if(isset($_POST['update_user']))
+    {
     $model->updateUser($_POST['id'], $_POST['username'], $_POST['email'], $_POST['role'], $_POST['status']);
-    header("Location: /Project personal/MVC/Controller/AdminUsersController.php");
+    header("Location: ../Controller/AdminUsersController.php");
     exit;
 }
 
 
 $editUser = null;
-if(isset($_GET['edit_id'])){
+
+if(isset($_GET['edit_id']))
+    {
     $editUser = $model->getUserById($_GET['edit_id']);
 }
 
