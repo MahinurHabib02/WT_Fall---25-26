49eee50a9519c3d91fb0545b1bf2a2ebe7403fdb
MahinurHabib02 <mahinurhabib@gmail.com>
2026-01-16 15:32:55 +0600

    admin task view updated and done

--- Project personal/MVC/Views/Admin/admin_tasks_view.php
+++ Project personal/MVC/Views/Admin/admin_tasks_view.php
@@ -15,7 +15,6 @@
               <a href="../Controller/AdminDashboardController.php">Dashboard</a>
                    <a href="../Controller/AdminUsersController.php">Manage Users</a>
                 <a href="../Controller/EventController.php">Manage Events</a>
-                  <a href="../Controller/RegistrationController.php">Registrations</a>
                   <a href="../Controller/TaskController.php">Manage Tasks</a>           
                      <a href="../Controller/AdminSettingsController.php">Settings</a>
              <a href="../../MVC/Controller/logout.php">Logout</a>
